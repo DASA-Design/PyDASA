3a5f2ecd9a44a8fe6a33e66104145e89be317d9f
Felipe Martin <4166437+phillipus85@users.noreply.github.com>
2025-12-30 22:39:10 -0500

    Uncomment import statements and clean up commented-out code in config.py

--- src/pydasa/utils/__init__.py
+++ src/pydasa/utils/__init__.py
@@ -1,6 +1,13 @@
-# from pydasa.dimensional.constants import (
-#     PHY_FDU_PREC_DT,
-#     COMPU_FDU_PREC_DT,
-#     SOFT_FDU_PREC_DT,
-#     DFLT_FDU_PREC_LT,
-# )
+from pydasa.dimensional.constants import (
+    PHY_FDU_PREC_DT,
+    COMPU_FDU_PREC_DT,
+    SOFT_FDU_PREC_DT,
+    DFLT_FDU_PREC_LT,
+)
+
+__all__ = [
+    "PHY_FDU_PREC_DT",
+    "COMPU_FDU_PREC_DT",
+    "SOFT_FDU_PREC_DT",
+    "DFLT_FDU_PREC_LT",
+]

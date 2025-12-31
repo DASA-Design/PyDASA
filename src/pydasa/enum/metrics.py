68a186f1ee74601edbafe6d2ffb3222b73446f46
Felipe Martin <4166437+phillipus85@users.noreply.github.com>
2025-12-30 21:56:29 -0500

    Add new configuration modules for enum: basis, conversion, domain, and metrics

---
+++ src/pydasa/enum/metrics.py
@@ -0,0 +1,11 @@
+﻿# -*- coding: utf-8 -*-
+"""
+Configuration module for...
+
+# TODO: Add description of the module.
+
+*IMPORTANT:* Based on the theory from:
+
+    # H.Gorter, *Dimensionalanalyse: Eine Theoririe der physikalischen Dimensionen mit Anwendungen*
+"""
+# TODO add UnitMeassure for the Variable/Parameter class

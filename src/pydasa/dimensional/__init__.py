6e58bda032f482281a6787ce890c5ace0c1a67ce
Felipe Martin <4166437+phillipus85@users.noreply.github.com>
2025-12-30 22:44:14 -0500

    Populate __init__.py in dimensional module and remove unused constants from utils module

--- src/pydasa/dimensional/__init__.py
+++ src/pydasa/dimensional/__init__.py
@@ -1,1 +1,24 @@
-﻿
+﻿# -*- coding: utf-8 -*-
+"""
+Package dimensional
+===========================================
+
+Dimensional analysis constants and utilities for *PyDASA*.
+"""
+
+# populate the __init__.py to simplify the imports
+from pydasa.dimensional.constants import (
+    PHY_FDU_PREC_DT,
+    COMPU_FDU_PREC_DT,
+    SOFT_FDU_PREC_DT,
+    DFLT_FDU_PREC_LT,
+)
+
+# Now sers can do:
+# from pydasa.dimensional import PHY_FDU_PREC_DT
+__all__ = [
+    "PHY_FDU_PREC_DT",
+    "COMPU_FDU_PREC_DT",
+    "SOFT_FDU_PREC_DT",
+    "DFLT_FDU_PREC_LT",
+]

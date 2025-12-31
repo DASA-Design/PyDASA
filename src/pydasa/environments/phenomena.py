b5b3398b5baa0fcf4036c53cecbcd0048d75e791
Felipe Martin <4166437+phillipus85@users.noreply.github.com>
2025-12-30 21:57:21 -0500

    Add new modules for sensitivity analysis and Monte Carlo simulations

---
+++ src/pydasa/environments/phenomena.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Configuration module for...
+
+# TODO: Add description of the module.
+# TODO: rethink main class name.
+
+Classes:
+    **DimProblem**: Main class for PyDASA library, coordinating dimensional analysis, sensitivity analysis, and framework management.
+
+*IMPORTANT:* Based on the theory from:
+
+    # H.Gorter, *Dimensionalanalyse: Eine Theoririe der physikalischen Dimensionen mit Anwendungen*
+"""
+# TODO add DimProblem class for PyDASA library
